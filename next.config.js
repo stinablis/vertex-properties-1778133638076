785a05975b0be87dede885ab4a27cbdeee6f2ba9
stinablis <info@stinablis.com>
2026-05-07 14:00:53 +0800

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  trailingSlash: true,
+  basePath: '/vertex-properties-1778133638076',
+  assetPrefix: '/vertex-properties-1778133638076',
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+};
+module.exports = nextConfig;
